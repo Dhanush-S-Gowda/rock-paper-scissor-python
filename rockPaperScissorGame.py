76c5b76e2196e338e22baf71e2d45ddc3727bcab
Dhanush S Gowda <141866615+Dhanush-S-Gowda@users.noreply.github.com>
2024-01-15 00:42:42 +0530

    Update rockPaperScissorGame.py

--- rockPaperScissorGame.py
+++ rockPaperScissorGame.py
@@ -9,64 +9,78 @@ welcome = '''
                             
 '''
 print(welcome)
-score = 0
+user_score = 0
+comp_score = 0
 while True:
     user_choice = input("Enter Rock(R), Paper(P), Scissors(S) \nEnter 'E' to exit\n -->: ")
     if user_choice == 'E':
         break
     rock = '''  
-        _______
-    ---'   ____)  
+        ______
+    ---'  ____)  
         (_____)  
         (_____)  
-        (____)
+        (_____)
     ---.__(___)  
     '''
 
     paper = '''  
         _______
-    ---'   ____)____  
-            ______)  
-            _______)  
-            _______)
-    ---.__________)  
+    ---' ______)____
+            ________)  
+            ________)  
+            ________)
+    ---.____________)  
     '''
 
     scissors = '''  
         _______
     ---'   ____)____  
-            ______)  
-        __________)  
-        (____)
+              ______)  
+          __________)  
+         (____)
     ---.__(___)  
     '''
-    print("You choose\n")
+    print("You choose")
     if user_choice == 'R':
+        print("Rock")
         print(rock)
     elif user_choice == 'P':
+        print("Paper")
         print(paper)
     elif user_choice == 'S':
+        print("Scissors")
         print(scissors)
     else:
         print("-------->    Wrong input     <---------\n")
         continue
     random_choice = random.randint(1, 3)
-    print("Computer choose\n")
+    print("Computer choose")
     if random_choice == 1:
         computer_choice = 'R'
+        print("Rock")
         print(rock)
     elif random_choice == 2:
         computer_choice = 'P'
+        print("Paper")
         print(paper)
     else:
         computer_choice = 'S'
+        print("Scissors")
         print(scissors)
 
     if(user_choice == computer_choice):
         print("Draw")
     elif((user_choice == 'R' and computer_choice == 'S') or (user_choice == 'S' and computer_choice == 'P') or (user_choice == 'P' and computer_choice == 'R')):
         print("You won!")
-        score += 1
+        user_score += 1
     else:
         print("Computer won!")
-    print(f"Your Score: {score}\n****************************************************************\n")
+        comp_score +=1
+    print(f"Your Score: {user_score}, computer Score: {comp_score}\n****************************************************************\n")
+if(user_score>comp_score):
+    print("You have won")
+elif(user_score<comp_score):
+    print("You have lost")
+else:
+    print("Its a draw!")
